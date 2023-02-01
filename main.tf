18c52f5b9753548c0748fd1f534daa41ee4d8be4
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-02-01 13:31:25 +0000

    feat: add azurerm_virtual_network_peering resource

--- main.tf
+++ main.tf
@@ -5,7 +5,12 @@
 # - vnet_subnets_name_ids - a map of subnet name to subnet resource id, e.g. use lookup(module.hub_virtual_networks["key"].vnet_subnets_name_id, "subnet1")
 module "hub_virtual_networks" {
   for_each = var.hub_virtual_networks
-  source = "https://github.com/Azure/terraform-azurerm-subnets"
-  version = "1.0.0"
+  source   = "https://github.com/Azure/terraform-azurerm-subnets"
+  version  = "1.0.0"
   # ... TODO add required inputs
+}
+
+resource "azurerm_virtual_network_peering" "hub_peering" {
+  for_each = local.hub_peering_map
+  # ... TODO resource properties
 }
