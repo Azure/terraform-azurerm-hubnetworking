4fd5ee9d164274d2a3bbdf4ee430956bb9e5a5dd
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-02-01 13:32:45 +0000

    Matt ffffff/initial tf (#1)

---
+++ main.tf
@@ -0,0 +1,16 @@
+
+# Module to create virtual networks and subnets
+# Useful outputs:
+# - vnet_id - the resource id of vnet
+# - vnet_subnets_name_ids - a map of subnet name to subnet resource id, e.g. use lookup(module.hub_virtual_networks["key"].vnet_subnets_name_id, "subnet1")
+module "hub_virtual_networks" {
+  for_each = var.hub_virtual_networks
+  source   = "https://github.com/Azure/terraform-azurerm-subnets"
+  version  = "1.0.0"
+  # ... TODO add required inputs
+}
+
+resource "azurerm_virtual_network_peering" "hub_peering" {
+  for_each = local.hub_peering_map
+  # ... TODO resource properties
+}
