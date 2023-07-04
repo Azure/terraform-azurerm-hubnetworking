f8608a76323093f20be9f7a75190bcf74e6235ee
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-07-04 10:28:29 +0100

    test: update testy

--- test/e2e/terraform_e2e_test.go
+++ test/e2e/terraform_e2e_test.go
@@ -1,13 +1,8 @@
 package e2e
 
 import (
-	"bytes"
-	"net/http"
-	"strings"
+	"net"
 	"testing"
-	"time"
-
-	"github.com/gruntwork-io/terratest/modules/retry"
 
 	test_helper "github.com/Azure/terraform-module-test-helper"
 	"github.com/gruntwork-io/terratest/modules/terraform"
@@ -18,24 +13,7 @@ func TestExmaples_startup(t *testing.T) {
 	test_helper.RunE2ETest(t, "../../", "examples/startup", terraform.Options{
 		Upgrade: true,
 	}, func(t *testing.T, output test_helper.TerraformOutput) {
-		testUrl := output["connectivity_test_url"].(string)
-		response := retry.DoWithRetry(t, "assert_network_connectivity", 5, time.Minute, func() (string, error) {
-			//#nosec G107
-			resp, err := http.Get(testUrl)
-			if err != nil {
-				return "", err
-			}
-			defer func() {
-				_ = resp.Body.Close()
-			}()
-			buf := new(bytes.Buffer)
-			_, err = buf.ReadFrom(resp.Body)
-			if err != nil {
-				return "", err
-			}
-			return buf.String(), nil
-		})
-		response = strings.TrimSuffix(response, "\n")
-		assert.Equal(t, "hello world", response)
+		pip := output["spoke2_pip"].(string)
+		assert.NotNil(t, net.ParseIP(pip))
 	})
 }
