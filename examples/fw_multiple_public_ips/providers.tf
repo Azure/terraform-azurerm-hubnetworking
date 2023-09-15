0716b45a72f2dee7c48c1e094a3e8bf7772e0fb7
zjhe <hezijie@microsoft.com>
2023-09-15 15:01:35 +0800

    remove unused providers to fix tflint issue

--- examples/fw_multiple_public_ips/providers.tf
+++ examples/fw_multiple_public_ips/providers.tf
@@ -6,17 +6,9 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">=3.7.0, < 4.0"
     }
-    local = {
-      source  = "hashicorp/local"
-      version = "2.3.0"
-    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.0"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "4.0.4"
     }
   }
 }
