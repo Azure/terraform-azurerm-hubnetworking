4d005bcf631736ad21a722e33b9431e9da049258
Ryan Royals <47660394+ryan-royals@users.noreply.github.com>
2024-06-17 19:10:03 +0930

    Add Tags to Route Table and Public IPs (#76)

--- variables.tf
+++ variables.tf
@@ -5,6 +5,7 @@ variable "hub_virtual_networks" {
     location                        = string
     resource_group_name             = string
     route_table_name                = optional(string)
+    route_table_tags                = optional(map(string))
     bgp_community                   = optional(string)
     ddos_protection_plan_id         = optional(string)
     dns_servers                     = optional(list(string))
@@ -71,6 +72,7 @@ variable "hub_virtual_networks" {
       zones                            = optional(list(string))
       default_ip_configuration = optional(object({
         name = optional(string)
+        tags = optional(map(string))
         public_ip_config = optional(object({
           ip_version = optional(string)
           name       = optional(string)
@@ -80,6 +82,7 @@ variable "hub_virtual_networks" {
       }))
       management_ip_configuration = optional(object({
         name = optional(string)
+        tags = optional(map(string))
         public_ip_config = optional(object({
           ip_version = optional(string)
           name       = optional(string)
@@ -114,6 +117,8 @@ A map of the hub virtual networks to create. The map key is an arbitrary value t
 - `routing_address_space` - A list of IPv4 address spaces in CIDR format that are used for routing to this hub, e.g. `["192.168.0.0","172.16.0.0/12"]`.
 - `hub_router_ip_address` - If not using Azure Firewall, this is the IP address of the hub router. This is used to create route table entries for other hub networks.
 - `tags` - A map of tags to apply to the virtual network.
+- `route_table_name` - The name of the route table to create for this hub network.
+- `route_table_tags` - A map of tags to apply to all route tables.
 
 #### Route table entries
 
@@ -157,13 +162,14 @@ A map of the hub virtual networks to create. The map key is an arbitrary value t
   - `name` - (Optional) The name of the firewall resource. If not specified will use `afw-{vnetname}`.
   - `private_ip_ranges` - (Optional) A list of private IP ranges to use for the Azure Firewall, to which the firewall will not NAT traffic. If not specified will use RFC1918.
   - `subnet_route_table_id` = (Optional) The resource id of the Route Table which should be associated with the Azure Firewall subnet. If not specified the module will assign the generated route table.
-  - `tags` - (Optional) A map of tags to apply to the Azure Firewall. If not specified
+  - `tags` - (Optional) A map of tags to apply to the Azure Firewall.
   - `threat_intel_mode` - (Optional) The threat intelligence mode for the Azure Firewall. Possible values include `Alert`, `Deny`, `Off`.
   - `zones` - (Optional) A list of availability zones to use for the Azure Firewall. If not specified will be `null`.
   - `default_ip_configuration` - (Optional) An object with the following fields. If not specified the defaults below will be used:
     - `name` - (Optional) The name of the default IP configuration. If not specified will use `default`.
     - `public_ip_config` - (Optional) An object with the following fields:
       - `name` - (Optional) The name of the public IP configuration. If not specified will use `pip-afw-{vnetname}`.
+      - `tags` - (Optional) A map of tags to apply to the public IP configuration.
       - `zones` - (Optional) A list of availability zones to use for the public IP configuration. If not specified will be `null`.
       - `ip_version` - (Optional) The IP version to use for the public IP configuration. Possible values include `IPv4`, `IPv6`. If not specified will be `IPv4`.
       - `sku_tier` - (Optional) The SKU tier to use for the public IP configuration. Possible values include `Regional`, `Global`. If not specified will be `Regional`.
@@ -171,6 +177,7 @@ A map of the hub virtual networks to create. The map key is an arbitrary value t
     - `name` - (Optional) The name of the management IP configuration. If not specified will use `defaultMgmt`.
     - `public_ip_config` - (Optional) An object with the following fields:
       - `name` - (Optional) The name of the public IP configuration. If not specified will use `pip-afw-mgmt-<Map Key>`.
+      - `tags` - (Optional) A map of tags to apply to the public IP configuration.
       - `zones` - (Optional) A list of availability zones to use for the public IP configuration. If not specified will be `null`.
       - `ip_version` - (Optional) The IP version to use for the public IP configuration. Possible values include `IPv4`, `IPv6`. If not specified will be `IPv4`.
       - `sku_tier` - (Optional) The SKU tier to use for the public IP configuration. Possible values include `Regional`, `Global`. If not specified will be `Regional`.
